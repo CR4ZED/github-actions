e543029f0c2c3172f3d6de9b2cd08916c0bab084
ankush <ankush.arvind@anywhere.co>
2022-03-07 16:23:07 +0530

    added new test cases

--- app.test.js
+++ app.test.js
@@ -5,4 +5,12 @@ describe("test cases for basic math functions", () => {
         const result = math.add(1,2)
         expect(result).toEqual(3)
     })
+    it("should sub 2 numbers", () => {
+        const result = math.sub(1,2)
+        expect(result).toEqual(-1)
+    })
+    it("should mul 2 numbers", () => {
+        const result = math.mul(1,2)
+        expect(result).toEqual(2)
+    })
 })
